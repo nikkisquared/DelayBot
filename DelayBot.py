8340539fc0a22784b74cc42711966fe220a322a5
Nikki <nikkicubed@gmail.com>
2015-03-24 18:40:09 -0400

    reformated main, respond, and split off some helpful functions

--- DelayBot.py
+++ DelayBot.py
@@ -6,7 +6,6 @@ import psycopg2
 import dataset
 import json, os, sys
 import time
-import datetime
 
 import timeconversions as TC
 import delaymessage as DM
@@ -79,44 +78,59 @@ class DelayBot(object):
         self.client.send_message(message)
 
 
+    def send_private_message(self, to, content):
+        """Simple wrapper for sending a private message on zulip"""
+        self.client.send_message({
+            "type": u"private",
+            "to": to,
+            "content": content
+        })
+
+
     def parse_destination(self, content, msg, private): 
         """
-        Parses and returns the stream and topic
+        Parses and returns the stream and topic, if there is any to get
         If the message is private, stream and topic must be user specified
         Otherwise, stream and topic can be taken from the msg metadata
         """
 
-        if private:
-            stream = content[0:0].replace("_", " ")
-            topic = content[1:1].replace("_", " ")
-        else:
+        if not private:
             stream = msg["display_recipient"]
             topic = msg["subject"]
+        elif len(content) < 4:
+            stream = ""
+            topic = ""
+        else:
+            stream = content[2].replace("_", " ")
+            topic = content[3].replace("_", " ")
 
         return stream, topic
 
 
     def is_valid_message(self, content, sender, private, stream):
+        """
+        Validates a given command to check if it can be parsed further
+        Raises a ValueError on fatally incorrect inputs
+        """
+
         activation_word = content[0].lower()
         command_terms = len(content)
-
+        terms_error = "Not enough commands given. You must specify a delay time"
 
         if activation_word != self.key_word:
             return False
         elif "delaybot" in sender.lower():
             return False
-        elif command_terms<3 or (private and command_terms<5):
-            return False
+        elif private and command_terms < 5:
+            raise ValueError(terms_error + ", stream, topic, and message "
+                            "when calling me from a private message.")
+        elif command_terms < 3:
+            raise ValueError(terms_error + " and message when calling me "
+                            "from public streams.")
         elif stream not in self.stream_names:
-            return False
-            #raise ValueError("There is no stream \"%s\"." % stream) 
+            raise ValueError("There is no stream \"%s\"." % stream) 
         else:
             return True
-
-            #raise ValueError("Not enough commands given. You must "
-            #            "specify a delay time, stream, topic, and message.")
-            #raise ValueError("Not enough commands given. You must "
-            #            "specify a delay time and message.")
             
 
     def respond(self, msg):  
@@ -124,14 +138,12 @@ class DelayBot(object):
 
         content = msg["content"].split(" ")
         private = (msg["type"] == "private")
-
-        stream, topic = self.parse_destination(content[2:], msg, private)
+        stream, topic = self.parse_destination(content, msg, private)
         
         if self.is_valid_message(content, msg["sender_full_name"], private, stream):
 
             timestamp, date = TC.parse_time(content[1], msg["timestamp"])
-            msg["content"] = "\nYou have delayed to: %s" % date
-            msg["content"] += "\nThe unix encoding for this is: %s" % timestamp
+            user_response = u"You have delayed a message to %s" % date
 
             # this refers to the start position of the message to be sent
             message_offset = 2
@@ -140,31 +152,29 @@ class DelayBot(object):
             dm = DM.delay_message(timestamp, msg["sender_full_name"],
                                 self.currentUid, stream, topic, message)
             self.currentUid += 1
-            # we should not be editing raw message throughout, create a new one
-            # nikki here: I know, I want to implement an error system instead
-            # of clumsily editing messages. this will do for now though...
-            self.send_message(msg) 
+
+            self.send_private_message(msg["sender_email"], user_response)
             self.add_message_to_db(dm)
 
 
-    def check_db(self, unix_timestamp ):
+    def check_db(self, unix_timestamp):
+        print unix_timestamp
         with dataset.connect() as db:
-            results = db.query("SELECT * FROM messages WHERE timestamp<%d"%unix_timestamp)
-            print results
+            results = db.query("SELECT * FROM messages WHERE timestamp<%d" % unix_timestamp)
+            #print results
             for result in results:
+                print result
                 msg = DM.create_message(result)
                 self.send_message(msg)
                 self.remove_message_from_db(result)
-
-                print result
-            for res in db["messages"].all(): 
-                print [ (x, res[x]) for x in res.keys()]
             db.commit()
 
 
     def add_message_to_db(self, delay_message):
         with dataset.connect() as db:
             db["messages"].insert(delay_message)
+            for res in db["messages"].all():
+                print [ (x, res[x]) for x in res.keys()]
             db.commit()
 
 
@@ -174,7 +184,7 @@ class DelayBot(object):
             db.commit()
 
 
-    def handle_error(e):
+    def handle_error(self, e, sender):
         """Handles everything with a given error to print it, and send it to the user"""
 
         error = e.message
@@ -182,32 +192,30 @@ class DelayBot(object):
         error = error.replace(" M", " Minute")
         error = error.replace(" S", " Second")
         print error
-        event["message"]["content"] = error
-        self.send_message(event["message"]) 
+        self.send_private_message(sender, error + u"")
 
 
     def main(self):
         """Blocking call that runs forever. Calls self.respond() on every message received."""
 
+        queue_id = None
         registration = self.client.register(json.dumps(["message"]))
-        queue_id = registration["queue_id"]
-        last_event_id = registration["last_event_id"]
+        queue_id = registration.get("queue_id")
+        last_event_id = registration.get("last_event_id")
 
         while True:
-
             results = self.client.get_events(queue_id=queue_id, 
                         last_event_id=last_event_id, dont_block=True)
-
             for event in results["events"]:
                 last_event_id = max(last_event_id, event["id"])
+
                 if "message" in event.keys():
                     try:
                         self.respond(event["message"])
                     except ValueError as e:
-                        self.handle_error(e)
+                        self.handle_error(e, event["message"]["sender_email"])
 
-            now = int(time.time()) 
-            #print now
+            now = int(time.time())
             self.check_db(now)
 
 
